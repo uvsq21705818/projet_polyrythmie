5b4e172e3c68d965c4a4f9367c36bf40ac5b7a88
Lucas <lucas.federspiel@ens.uvsq.com>
2022-03-10 23:38:38 +0100

    modif du code pour avoir la vitesse selon le temps

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ import random
 WIDTH = 600
 HEIGHT = 600
 theta = (-1) * math.pi / 2
-temps = 50
-rayon_cercle = (WIDTH - 200) / 2
+t0 = 3
+temps = 0
+rayon_cercle = (min(WIDTH, HEIGHT) / 2) / 2
+omega = math.pi / 2
+dt = 0.01 #s
 
 #COULEURS (c'est juste pour le style)
 LISTE_COULEUR =["#D6ECFF", "#CCE8FF", "#C5E5FF", "#BFE2FF", "#B8DFFF", "#A8D7FF", "#A1D4FF", "#95CFFF", "#8ACAFF", "#80C5FF"]
@@ -16,25 +19,25 @@ root = tk.Tk()
 root.title("aaaaaaaaaaaaaaaaaaa")
 
 screen = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="black")
-cercle = screen.create_oval(100, 100, WIDTH - 100, HEIGHT - 100, outline="#2FA0FF")
+cercle = screen.create_oval(((WIDTH / 2) - rayon_cercle), ((HEIGHT / 2) - rayon_cercle), ((WIDTH / 2) + rayon_cercle), ((HEIGHT / 2) + rayon_cercle), outline="#2FA0FF")
 taille_objet = 20
-objet_temps = screen.create_oval((WIDTH/2) - (taille_objet/2), (HEIGHT - 100) - (taille_objet/2), (WIDTH/2) + (taille_objet/2), (HEIGHT - 100) + (taille_objet/2), fill="#EAF3FB", outline="#A2B5C7")
+objet_temps = screen.create_oval((WIDTH/2) - (taille_objet/2), ((HEIGHT/2) + rayon_cercle) - (taille_objet/2), (WIDTH/2) + (taille_objet/2), ((HEIGHT/2) + rayon_cercle) + (taille_objet/2), fill="#EAF3FB", outline="#A2B5C7")
 
-def coord_temps(theta):
-    x = rayon_cercle * math.cos(theta)
-    y = rayon_cercle * math.sin(theta)
-
+def coord_temps(temps):
+    x = (WIDTH / 2) + rayon_cercle * math.cos(omega * (t0 + temps))
+    y = (HEIGHT / 2) - rayon_cercle * math.sin(omega * (t0 + temps))
     return x, y
 
 def move():
-    global theta
+    global temps
 
-    theta += math.pi / temps
-    dx = (-1) * (coord_temps(theta)[0] - coord_temps(theta - math.pi / temps)[0])
-    dy = (-1) * (coord_temps(theta)[1] - coord_temps(theta - math.pi / temps)[1])
+    #print((omega * temps)/(math.pi*1000))
+    dx = (coord_temps(temps + dt)[0] - coord_temps(temps)[0])
+    dy = (coord_temps(temps + dt)[1] - coord_temps(temps)[1])
+    print(math.cos(omega * temps), math.sin(omega * temps))
     screen.move(objet_temps, dx, dy)
-
-    screen.after(40, move)
+    temps += dt
+    screen.after(int(dt*1000), move)
 
 
 def trace_polyrythme(NOMBRE_DE_RYTHME):
@@ -50,8 +53,6 @@ def trace_polyrythme(NOMBRE_DE_RYTHME):
         screen.create_line((WIDTH/2) + liste_points[i-1][0], (HEIGHT/2) + liste_points[i-1][1], (WIDTH/2) + liste_points[i][0], (HEIGHT/2) + liste_points[i][1], fill=color)
 
 move()
-for i in range(3, 10):
-    trace_polyrythme(i)
 
 screen.grid(column=0, row=0, rowspan=4)
 
