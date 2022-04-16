7da80f92b47db850e2815c87f980288539f9fe65
Lucas <lucas.federspiel@ens.uvsq.com>
2022-04-16 21:35:17 +0200

    grosse modif de la fonction move et de trace_poly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #NK####
 
+from tempfile import tempdir
 import tkinter as tk
 import math
 import random
@@ -10,15 +11,9 @@ WIDTH = 700
 HEIGHT = 700
 temps = 0
 rayon_cercle = (min(WIDTH, HEIGHT) / 2) / 2
-<<<<<<< HEAD
-omega = math.pi / 8 #vitesse angulaire
-theta0 = (3 * math.pi) / 2 #angle de départ
-dt = 0.02 #secondes
-=======
 omega = math.pi / 2
 omega0 = (3 * math.pi) / 2
-dt = 0.01 #secondes
->>>>>>> 500675b5826dd6477c3fc4ca70f586043b40eb09
+dt = 0.001 #secondes
 taille_objet = 20
 temps_rotation = (2 * math.pi) / omega #secondes
 tours = 0
@@ -32,10 +27,11 @@ LISTE_COULEUR =["#D6ECFF", "#CCE8FF", "#C5E5FF", "#BFE2FF", "#B8DFFF", "#A8D7FF"
 
 def coord_temps(temps):
     """Calcule les coordonnées de la boule à un instant donné"""
-    x = (WIDTH / 2) + rayon_cercle * math.cos((omega * temps) + theta0)
+    x = (WIDTH / 2) + rayon_cercle * math.cos((omega * temps) + omega0)
     y = (HEIGHT / 2) + rayon_cercle * math.sin((omega * temps) + math.pi/2)
 
     return x, y
+
 
 def move():
     """Permet de faire bouger la boule du temps"""
@@ -70,14 +66,34 @@ def trace_polyrythme(NOMBRE_DE_RYTHME):
         alpha += (2 * math.pi) / NOMBRE_DE_RYTHME
         n +=1
 
-    for i in range(len(liste_points)):
+    nombre_sommet = len(liste_points)
+
+    nombre_de_coordonnees_rythme = temps_rotation // dt
+
+    ### Pour n'avoir aucun décalage il faut le nombre de coordonées et le nobre de sommet soit entiers entre eux:
+    while (nombre_de_coordonnees_rythme % nombre_sommet) != 0:
+        nombre_de_coordonnees_rythme +=1
+
+    nombre_de_coordonnees_segment = int(nombre_de_coordonnees_rythme / nombre_sommet)
+    liste_coord = []
+
+    for i in range(nombre_sommet):
         screen.create_line(liste_points[i-1][0], liste_points[i-1][1], liste_points[i][0], liste_points[i][1], fill=color)
 
-    objet = screen.create_oval((WIDTH/2) - (taille_objet/2), ((HEIGHT/2) + rayon_cercle) - (taille_objet/2), (WIDTH/2) + (taille_objet/2), ((HEIGHT/2) + rayon_cercle) + (taille_objet/2), fill="#A2B5C7")
-    liste_points.insert(0, objet)
-    liste_points.insert(1, 0)
+        for j in range(nombre_de_coordonnees_segment):
+            if i == len(liste_points)-1:
+                coord_x = liste_points[i][0] + (j/nombre_de_coordonnees_segment) * (liste_points[0][0] - liste_points[i][0])
+                coord_y = liste_points[i][1] + (j/nombre_de_coordonnees_segment) * (liste_points[0][1] - liste_points[i][1])
+                liste_coord.append([coord_x, coord_y])
+            else:
+                coord_x = liste_points[i][0] + (j/nombre_de_coordonnees_segment) * (liste_points[i+1][0] - liste_points[i][0])
+                coord_y = liste_points[i][1] + (j/nombre_de_coordonnees_segment) * (liste_points[i+1][1] - liste_points[i][1])
+                liste_coord.append([coord_x, coord_y])
 
-    LISTE_POLYRYTHMES.append(liste_points)
+    objet = screen.create_oval((WIDTH/2) - (taille_objet/2), ((HEIGHT/2) + rayon_cercle) - (taille_objet/2), (WIDTH/2) + (taille_objet/2), ((HEIGHT/2) + rayon_cercle) + (taille_objet/2), fill="#A2B5C7")
+    liste_rythme = [objet, 0, liste_coord, liste_points]
+
+    LISTE_POLYRYTHMES.append(liste_rythme)
 
 def pause():
     global is_paused
@@ -92,65 +108,48 @@ def pause():
 class Rythme:
 
     def __init__(self, liste):
-        """Ici notre objet rythme sera une liste sous la forme [A, K, [x1, y1], ..., [xn, yn]] avec A l'objet associé à ce rythme,
-        K le numéro du segment ou se trouve l'objet et [x, y] les coordonées de chaques sommets"""
+        """Ici notre objet rythme sera une liste sous la forme [A, N, C, S] avec :
+        # A l'objet associé à ce rythme,
+        # N le numéro des coordonées ou se trouve l'objet (par rapport à la liste des coordonnées)
+        # C la liste de toutes les coordonées parcourues par l'objet (sous la forme [[x,y], [x,y], ..., [x,y]])
+        # S les coordonées de chaques sommets (sous la forme [[x1, y1], ..., [xn, yn]])"""
 
-        liste_points = liste.copy()
-        liste_points.pop(0)
-        liste_points.pop(0)
+        liste_points = liste[3].copy()
+
         nb_points = len(liste_points)
 
+
         objet = liste[0]
-        segment = liste[1]
+        numero_coordonnee = liste[1]
+        liste_coordonees = liste[2]
 
         self.nb_points= nb_points
         self.liste_points = liste_points
         self.objet = objet
-        self.segment = segment
+        self.liste_coordonees = liste_coordonees
+        self.numero_coordonee = numero_coordonnee
 
     def move(self, liste):
         global LISTE_POLYRYTHMES
 
-        t_segment = temps_rotation / self.nb_points
-        print(self.segment)
-        if self.segment == 0:
-            x = self.liste_points[1][0] - self.liste_points[0][0]
-            y = self.liste_points[1][1] - self.liste_points[0][1]
-            liste[1] = 1
+        if self.numero_coordonee == len(self.liste_coordonees):
+            x = self.liste_coordonees[0][0]
+            y = self.liste_coordonees[0][1]
+
+            liste[1] = 0
+
         else:
-            for i in range(self.nb_points + 1):
-                if self.segment == i:
-                
-                    if (temps - temps_rotation * tours) / i <= temps_rotation / self.nb_points and i != self.nb_points:
-                        x = self.liste_points[i][0] - self.liste_points[i-1][0]
-                        y = self.liste_points[i][1] - self.liste_points[i-1][1]
-                    else:
-            
+            x = self.liste_coordonees[self.numero_coordonee][0]
+            y = self.liste_coordonees[self.numero_coordonee][1]
 
-                        if i == self.nb_points - 1:
-                            x = self.liste_points[0][0] - self.liste_points[i][0]
-                            y = self.liste_points[0][1] - self.liste_points[i][1]
+            liste[1] += 1
 
-                            liste[1] += 1
-                        
-                        elif i == self.nb_points:
-                            x = self.liste_points[0][0] - self.liste_points[i-1][0]
-                            y = self.liste_points[0][1] - self.liste_points[i-1][1]
+        x1 = x - (taille_objet / 2)
+        y1 = y - (taille_objet / 2)
+        x2 = x + (taille_objet / 2)
+        y2 = y + (taille_objet / 2)
 
-                            if (temps - temps_rotation * tours) > temps_rotation:
-                                liste[1] = 1
-
-                        else:
-                            x = self.liste_points[i+1][0] - self.liste_points[i][0]
-                            y = self.liste_points[i+1][1] - self.liste_points[i][1]
-                            liste[1] += 1           
-
-
-        dx = (x * dt) / t_segment
-        dy = (y * dt) / t_segment
-
-        screen.move(self.objet, dx, dy)
-
+        screen.coords(self.objet, x1, y1, x2, y2)
 
     
 
@@ -166,13 +165,8 @@ cercle = screen.create_oval(((WIDTH / 2) - rayon_cercle), ((HEIGHT / 2) - rayon_
 objet_temps = screen.create_oval((WIDTH/2) - (taille_objet/2), ((HEIGHT/2) + rayon_cercle) - (taille_objet/2), (WIDTH/2) + (taille_objet/2), ((HEIGHT/2) + rayon_cercle) + (taille_objet/2), fill="#EAF3FB", outline="#A2B5C7")
 bouton_pause = tk.Button(text="PAUSE", command=pause)
 
-<<<<<<< HEAD
-#for i in range(3,7):
-    #trace_polyrythme(i)
-trace_polyrythme(3)
-=======
-trace_polyrythme(5)
->>>>>>> 500675b5826dd6477c3fc4ca70f586043b40eb09
+for i in range(2,10):
+    trace_polyrythme(i)
 move()
 
 screen.grid(column=0, row=0)
