2ada0dcbf0283b569c2702ade90b29f310358c60
Lucas <lucas.federspiel@ens.uvsq.com>
2022-05-31 22:05:31 +0200

    correction du décalage entre rythmes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ HEIGHT = 600
 temps = 0
 rayon_cercle = (min(WIDTH, HEIGHT) / 2) * 0.75
 centre = (WIDTH/2, HEIGHT/2)
-vitesse = 5
+vitesse = 3
 omega = (math.pi / 2) * vitesse
 theta0 = (3 * math.pi) / 2
 dt = 0.001 #secondes
@@ -22,11 +22,11 @@ periode = (2 * math.pi) / omega #secondes
 tours = 0
 LISTE_POLYRYTHMES = []
 is_paused = False
-acceleration = 10 #mesure arbitraire de l'accélération d'une boule d'un rythme sur un segment
+acceleration = 0 #mesure arbitraire de l'accélération d'une boule d'un rythme sur un segment
 vitesse_trainee = 2
 compteur_dt = 0
-longueur_trainee = 10
-durée_grossissement = 40
+longueur_trainee = 0
+durée_grossissement = 0
 
 #COULEURS (c'est juste pour le style)#
 LISTE_COULEUR =["orange", "blue", "pink", "purple", "green", "yellow", "red"]
@@ -53,6 +53,7 @@ def move_temps():
             Rythme(LISTE_POLYRYTHMES[i]).move_rythm(LISTE_POLYRYTHMES[i])
             if compteur_dt == vitesse_trainee:
                 Rythme(LISTE_POLYRYTHMES[i]).trainee()
+
                 
         if compteur_dt == vitesse_trainee:
             compteur_dt = 0
@@ -83,38 +84,70 @@ def trace_rythme(NOMBRE_DE_RYTHME):
         alpha += (2 * math.pi) / NOMBRE_DE_RYTHME
         n +=1
 
-    print(coord_sommet)
+
 
     
     nb_sommet = len(coord_sommet)
     #nombre de positions que peut prendre une boule pour un rythme en un tour
-    nb_positions_rythme = periode // dt 
-
-    ### Pour n'avoir aucun décalage il faut le nombre de coordonées et le nobre de sommet soit entiers entre eux:
-    while (nb_positions_rythme % nb_sommet) != 0:
-        nb_positions_rythme +=1
+    nb_positions_rythme = periode // dt
 
     #nb de positions pour un segment
     nb_coord_1segment = int(nb_positions_rythme / nb_sommet)
+
+    # Ici le problème est que en divisant le nombre total de coordonnées par le nombre de sommet pour avoir le nombre
+    # de coordonnées dans un seul segment, nous devons avoir un nombre entier (int) de coordonnées pour 1 segment cependant comme on arrondit
+    # cela modifie aussi le nombre total de coordonnées.
+    # On peut donc se retrouver avec 2 rythmes qui on un nombre différent de coordonnées pour faire un tour du cercle
+    # (par ex: 1330 pour un Rythme de 4 et 1332 pour un Rythme de 3).
+    # Pour palier à cette différence (minime mais qui engendrera un léger décalage des objets à chaque tours),
+    # on enregistre la différence de coordonnées par rapport à la base de coordonnées qu'on devrait avoir (nb_positions_rythme).
+    # Cette différence sera en suite ajoutée aux coordonnées du dernier segment pour rééquilibrer les objets.
+    #
+    # Exemple: La base de coordonnées pour un tour est de 1331
+    #
+    # # L'objet du Rythme 4 possède un total de 1330 coordonnée 
+    # # On ajoute donc 1 coordonnée au dernier segment (correction_nombre_coordonnee = 1)
+    #
+    # # L'objet du Rythme 3 possède un total de 1332 coordonnée
+    # # On enlève donc 1 coordonnée au dernier segment (correction_nombre_coordonnee = -1)
+
+    if nb_coord_1segment * nb_sommet != int(nb_positions_rythme):
+        correction_nombre_coordonnee = int(nb_positions_rythme) - nb_coord_1segment * nb_sommet
+
     liste_coord = []
 
     for i in range(nb_sommet):
         screen.create_line(coord_sommet[i-1][0], coord_sommet[i-1][1], coord_sommet[i][0], coord_sommet[i][1], fill=color)
         screen.create_line(centre[0], centre[1], coord_sommet[i-1][0], coord_sommet[i-1][1], fill="gray")
 
-        for j in range(nb_coord_1segment):
+        if i == nb_sommet-1 and correction_nombre_coordonnee != 0:
             
-            #facteur
-            accel = (math.exp(acceleration * ((j+1)/nb_coord_1segment)) / math.exp(acceleration)) * ((j+1) / nb_coord_1segment)
+            for j in range(nb_coord_1segment + correction_nombre_coordonnee):
 
-            if i == nb_sommet-1:
+                accel = (math.exp(acceleration * ((j+1)/nb_coord_1segment)) / math.exp(acceleration)) * ((j+1) / nb_coord_1segment)
+
                 coord_x = coord_sommet[i][0] + (coord_sommet[0][0] - coord_sommet[i][0]) * accel
                 coord_y = coord_sommet[i][1] + (coord_sommet[0][1] - coord_sommet[i][1]) * accel
                 liste_coord.append([coord_x, coord_y])
-            else:
-                coord_x = coord_sommet[i][0] + (coord_sommet[i+1][0] - coord_sommet[i][0]) * accel
-                coord_y = coord_sommet[i][1] + (coord_sommet[i+1][1] - coord_sommet[i][1]) * accel
-                liste_coord.append([coord_x, coord_y])
+
+
+        else:
+            for j in range(nb_coord_1segment):
+                
+                #facteur
+                accel = (math.exp(acceleration * ((j+1)/nb_coord_1segment)) / math.exp(acceleration)) * ((j+1) / nb_coord_1segment)
+
+                if i == nb_sommet-1:
+                    
+                    coord_x = coord_sommet[i][0] + (coord_sommet[0][0] - coord_sommet[i][0]) * accel
+                    coord_y = coord_sommet[i][1] + (coord_sommet[0][1] - coord_sommet[i][1]) * accel
+                    liste_coord.append([coord_x, coord_y])
+
+                else:
+
+                    coord_x = coord_sommet[i][0] + (coord_sommet[i+1][0] - coord_sommet[i][0]) * accel
+                    coord_y = coord_sommet[i][1] + (coord_sommet[i+1][1] - coord_sommet[i][1]) * accel
+                    liste_coord.append([coord_x, coord_y])
 
     objet = screen.create_oval(centre[0] - (taille_objet/2), (centre[1] + rayon_cercle) - (taille_objet/2), centre[0] + (taille_objet/2), (centre[1] + rayon_cercle) + (taille_objet/2), fill=color)
     liste_rythme = [objet, 0, liste_coord, coord_sommet, [], color]
@@ -274,9 +307,6 @@ class Rythme:
         screen.coords(objet, x0, y0, x1, y1)
 
 
-
-
-
 #-#-# Boucle Tkinter #-#-#
 
 root = tk.Tk()
@@ -287,7 +317,7 @@ cercle = screen.create_oval((centre[0] - rayon_cercle), (centre[1] - rayon_cercl
 objet_temps = screen.create_oval(centre[0] - (taille_objet/2), (centre[1] + rayon_cercle) - (taille_objet/2), centre[0] + (taille_objet/2), (centre[1] + rayon_cercle) + (taille_objet/2), fill="#EAF3FB", outline="#A2B5C7")
 bouton_pause = tk.Button(text="PAUSE", command=pause)
 
-for i in range(3,6):
+for i in range(3,5):
     trace_rythme(i)
 move_temps()
 
